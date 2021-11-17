fe118e6f996942f0fa33bec8521a23713683fe94
dejwtoziomal <dawidmasalski@icloud.com>
2021-11-17 19:29:59 +0100

    Added gallery

---
+++ js/gallery.js
@@ -0,0 +1,53 @@
+let imgCount = 2;
+let imgNow = 0;
+
+function openImage(el) {
+    document.getElementById('fullscreen').style.display = "flex";
+    document.getElementById('fullscreenImg').src = el.src;
+
+    imgNow = el.src.substring(el.src.length - 5, el.src.length - 4);
+}
+
+function closeImage() {
+    document.getElementById('fullscreen').style.display = "none";
+}
+
+function nextImage() {
+    imgNow++;
+
+    if(imgNow > imgCount)
+        imgNow = 1;
+
+    changeImage();
+}
+
+function prevImage() {
+    imgNow--;
+
+    if(imgNow < 1)
+        imgNow = imgCount;
+
+    changeImage();
+}
+
+function changeImage() {
+    document.getElementById('fullscreenImg').src = "./img/gallery/" + imgNow + ".jpg";
+}
+
+document.addEventListener('keydown', checkKey);
+
+function checkKey(e) {
+    if(document.getElementById('fullscreen').style.display == "flex") {
+        e = e || window.event;
+
+        if (e.keyCode == '37') {
+            prevImage();
+        }
+        else if (e.keyCode == '39') {
+            nextImage();
+        }
+        else if (e.keyCode == '27') {
+            closeImage();
+        }
+    }
+}
